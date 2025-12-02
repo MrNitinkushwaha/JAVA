341611bb56fbfc9b9273001136645e9eaa4910a0
nitin kushwaha <nitin348926@gmail.com>
2025-12-02 16:05:07 +0530

    DAY 1

---
+++ DAY 1/input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class input {
+     public static void main(String args[]){
+    //  int a=12;
+    //  int b=13;
+    //  int c=a+b;
+    //  System.out.println(c);
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      int c=a+b;
+      System.out.println(c);
+
+}}
